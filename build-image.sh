71a487a3323d43dda53ba6c3cc0be1f12097a0c1
Guy Balmas <guyb@terasky.com>
2025-01-20 22:10:43 +0200

    + update

--- build-image.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-docker build -t docker.io/guybalmas/temporary-rbac-controller:v1.0.13 .
-docker login
-docker push docker.io/guybalmas/temporary-rbac-controller:v1.0.13
-
-kubectl delete -f /mnt/c/Users/GuyBalmas/IdeaProjects/teraky-int/tarbac-controller/config/manager
-kubectl apply -f /mnt/c/Users/GuyBalmas/IdeaProjects/teraky-int/tarbac-controller/config/manager
