1c0c0617bf12203d4547eb56a5f2b9e07104ef75
Guy Balmas <guyb@terasky.com>
2025-01-26 13:26:48 +0200

    + Log formatting

--- utils/helpers.go
+++ utils/helpers.go
@@ -17,13 +17,21 @@ func truncate(input string, maxLength int) string {
 	return input
 }
 
+func trimUID(uid string, length int) string {
+	trimmedUID := uid
+	if len(uid) > length {
+		trimmedUID = uid[len(uid)-length:]
+	}
+	return trimmedUID
+}
+
 // GenerateBindingName generates a unique name for the binding, limited to 63 characters.
 func GenerateBindingName(subject rbacv1.Subject, roleRef rbacv1.RoleRef, uid string) string {
 	// Trim components to fit within 63 characters.
 	kind := truncate(strings.ToLower(subject.Kind), 10) // Max 10 characters for Kind.
 	name := truncate(subject.Name, 20)                  // Max 20 characters for Subject Name.
 	role := truncate(roleRef.Name, 20)                  // Max 20 characters for Role Name.
-	trimmedUID := truncate(uid, 12)                     // Always use the last 12 characters of UID.
+	trimmedUID := trimUID(uid, 12)                      // Always use the last 12 characters of UID.
 	return fmt.Sprintf("%s-%s-%s-%s", kind, name, role, trimmedUID)
 }
 
@@ -33,48 +41,34 @@ func GenerateTempRBACName(subject rbacv1.Subject, sudoPolicy string, uid string)
 	kind := truncate(strings.ToLower(subject.Kind), 10) // Max 10 characters for Kind.
 	name := truncate(subject.Name, 20)                  // Max 20 characters for Subject Name.
 	policy := truncate(sudoPolicy, 20)                  // Max 20 characters for SudoPolicy Name.
-	trimmedUID := truncate(uid, 12)                     // Always use the last 12 characters of UID.
+	trimmedUID := trimUID(uid, 12)                      // Always use the last 12 characters of UID.
 	return fmt.Sprintf("%s-%s-%s-%s", kind, name, policy, trimmedUID)
 }
-
-// LogInfo standardizes info logging across controllers.
-// func LogInfoUID(logger logr.Logger, message string, controller string, kind string, resourceName string, namespace string, requestID string) {
-// 	logger.Info(message,
-// 		"controller", controller,
-// 		"controllerKind", kind,
-// 		kind, map[string]string{"name": resourceName},
-// 		"namespace", namespace,
-// 		"name", resourceName,
-// 		"requestID", requestID,
-// 	)
-// }
 
 func LogInfoUID(logger logr.Logger, message string, requestID string, additionalFields ...interface{}) {
 	fields := append([]interface{}{"requestID", requestID}, additionalFields...)
 	logger.Info(message,
 		fields...,
-	// "controllerKind", kind,
-	// kind, map[string]string{"name": resourceName},
-	// "namespace", namespace,
-	// "name", resourceName,
 	)
 }
 
-func LogInfo(logger logr.Logger, message string) {
-	logger.Info(message) // "controller", controller,
-	// "controllerKind", kind,
-	// kind, map[string]string{"name": resourceName},
-	// "namespace", namespace,
-	// "name", resourceName,
+func LogInfo(logger logr.Logger, message string, additionalFields ...interface{}) {
+	logger.Info(message,
+		additionalFields...,
+	)
 }
 
 // LogError standardizes error logging across controllers.
 func LogErrorUID(logger logr.Logger, err error, message string, requestID string, additionalFields ...interface{}) {
 	fields := append([]interface{}{"requestID", requestID}, additionalFields...)
-	logger.Error(err, message, fields...,
+	logger.Error(err,
+		message,
+		fields...,
 	)
 }
 
-func LogError(logger logr.Logger, err error, message string) {
-	logger.Error(err, message)
+func LogError(logger logr.Logger, err error, message string, additionalFields ...interface{}) {
+	logger.Error(err,
+		message,
+		additionalFields...)
 }
